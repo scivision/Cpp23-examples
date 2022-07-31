80ba42f98d23d188c3dd017a884bd7ada47034e9
Michael Hirsch <scivision@users.noreply.github.com>
2022-07-31 19:05:33 -0400

    visual studio stdlib import: need specific installer option

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -1,16 +1,30 @@
 include(CheckSourceCompiles)
 include(CheckCXXSymbolExists)
 
+
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED true)
+
+add_compile_definitions($<$<BOOL:${MSVC}>:_CRT_SECURE_NO_WARNINGS>)
+
+# --- modules
+
+add_compile_options("$<$<COMPILE_LANG_AND_ID:CXX,MSVC>:/experimental:module;/std:c++latest;/EHsc;/MD>")
+
+if(MSVC)
+  # can't use get_property(DIRECTORY COMPILE_OPTIONS) due to generator expressions not yet evaluated
+  set(CMAKE_REQUIRED_FLAGS /experimental:module /std:c++latest /EHsc /MD)
+endif()
+
 check_source_compiles(CXX
 [=[
-#include <cstdlib>
-#include <thread>
-int main() {
-    unsigned int n = std::thread::hardware_concurrency();
-    return EXIT_SUCCESS;
+import std.core;
+int main(){
+  std::cout << "hello" << std::endl;
+  return 0;
 }
 ]=]
-HAS_CXX_THREAD
+HAVE_MSVC_STDLIB_MODULES
 )
 
 
@@ -22,7 +36,18 @@ check_cxx_symbol_exists(__cpp_lib_math_constants numbers HAVE_CXX20_NUMBERS)
 
 check_cxx_symbol_exists(__cpp_modules "" HAVE_CXX20_MODULES)
 
-add_compile_definitions($<$<BOOL:${MSVC}>:_CRT_SECURE_NO_WARNINGS>)
+
+check_source_compiles(CXX
+[=[
+#include <cstdlib>
+#include <thread>
+int main() {
+    unsigned int n = std::thread::hardware_concurrency();
+    return EXIT_SUCCESS;
+}
+]=]
+HAVE_CXX_THREAD
+)
 
 # --- auto-ignore build directory
 if(NOT EXISTS ${PROJECT_BINARY_DIR}/.gitignore)
---
+++ modules/hello.cpp
@@ -0,0 +1,11 @@
+// This "import std" syntax is Microsoft particular--will it be accepted into standard?
+// https://docs.microsoft.com/en-us/cpp/cpp/modules-cpp?view=msvc-170#consume-the-c-standard-library-as-modules
+
+import std.core;
+
+int main(){
+
+  std::cout << "hello" << std::endl;
+  return 0;
+
+}
