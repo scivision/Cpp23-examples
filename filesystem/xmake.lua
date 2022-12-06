57f1c725c6f4c3eb52ce455a20cd4fc8d10435e3
Arthur LAURENT <arthur.laurent4@gmail.com>
2022-12-07 00:56:35 +0100

    fix tests

--- filesystem/xmake.lua
+++ filesystem/xmake.lua
@@ -9,14 +9,7 @@ target("canonical_demo")
     add_files("canonical_demo.cxx", "canonical.cxx")
     add_headerfiles("canonical_demo.hpp")
 
-local buildir = "$(buildir)"
-
 target("symlink")
     set_kind("binary")
     set_languages("c++17", "clatest")
     add_files("is_symlink.cpp")
-
-    -- on_run(function(target)
-        -- print("AAAAAAAAAAAAAAAA", path.join(target:scriptdir(), "is_symlink.cpp"), path.join(path.absolute(buildir), "test.lnk"))
-        -- os.runv(target:targetfile(), {path.join(target:scriptdir(), "is_symlink.cpp")}, {path.join(path.absolute(buildir), "test.lnk")})
-    -- end)
