377ba0b9993346c0946ee50955d72e8952791c08
Michael Hirsch <scivision@users.noreply.github.com>
2022-12-07 15:49:06 -0500

    cleanup comments [skip ci]

--- modules/hello.cpp
+++ modules/hello.cpp
@@ -1,4 +1,3 @@
-// This "import std" syntax is Microsoft particular--will it be accepted into standard?
 // https://docs.microsoft.com/en-us/cpp/cpp/modules-cpp?view=msvc-170#consume-the-c-standard-library-as-modules
 
 import std.core;
