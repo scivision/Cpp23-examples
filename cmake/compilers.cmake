6ae22c1767590f88f8e75d90acbecb3a21e6e504
scivision <scivision@users.noreply.github.com>
2023-02-13 00:18:30 -0500

    check with -Werror

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -6,14 +6,14 @@ add_compile_definitions($<$<BOOL:${MSVC}>:_CRT_SECURE_NO_WARNINGS>)
 
 if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
   add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:/experimental:module;/std:c++latest;/EHsc>")
-  set(CMAKE_REQUIRED_FLAGS /experimental:module /std:c++latest /EHsc)
+  list(APPEND CMAKE_REQUIRED_FLAGS /experimental:module /std:c++latest /EHsc)
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU" AND CMAKE_CXX_COMPILER_VERSION VERSION_GREATER_EQUAL 11)
   add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:-fmodules-ts>")
-  set(CMAKE_REQUIRED_FLAGS -fmodules-ts)
+  list(APPEND CMAKE_REQUIRED_FLAGS -fmodules-ts)
   # -x c++-system-header not work
 elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
   add_compile_options("$<$<COMPILE_LANGUAGE:CXX>:-fmodules>")
-  set(CMAKE_REQUIRED_FLAGS -fmodules)
+  list(APPEND CMAKE_REQUIRED_FLAGS -fmodules)
 endif()
 
 message(VERBOSE "CMAKE_REQUIRED_FLAGS: ${CMAKE_REQUIRED_FLAGS}")
