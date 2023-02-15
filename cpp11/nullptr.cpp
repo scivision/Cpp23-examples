a9cde5978f54deb009ac3584f765fb70b4019118
scivision <scivision@users.noreply.github.com>
2023-02-14 21:56:54 -0500

    add nullptr

---
+++ cpp11/nullptr.cpp
@@ -0,0 +1,25 @@
+// https://en.cppreference.com/w/cpp/language/nullptr
+#include <cstddef>
+#include <iostream>
+
+template<class T>
+constexpr T clone(const T& t)
+{
+    return t;
+}
+
+void g(int*)
+{
+    std::cout << "Function g called\n";
+}
+
+int main()
+{
+    g(nullptr);        // Fine
+    g(NULL);           // Fine
+    g(0);              // Fine
+
+    g(clone(nullptr)); // Fine
+//  g(clone(NULL));    // ERROR: non-literal zero cannot be a null pointer constant
+//  g(clone(0));       // ERROR: non-literal zero cannot be a null pointer constant
+}
