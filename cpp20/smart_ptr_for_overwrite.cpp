77f2546e3ba0e349ed073b57fc737e7f87571ace
scivision <scivision@users.noreply.github.com>
2023-08-12 23:02:24 -0400

    add smart pointer

---
+++ cpp20/smart_ptr_for_overwrite.cpp
@@ -0,0 +1,25 @@
+#include <memory>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <cstdlib>
+
+std::string append_hello(std::string s){
+  // many functions require char; this is a toy example
+  // showing use of std::make_unique<char[]> as a safe buffer
+
+  auto c1 = std::make_unique<char[]>(s.size() + 6);
+
+  std::strcpy(c1.get(), s.c_str());
+
+  std::strcat(c1.get(), "hello");
+
+  return std::string(c1.get());
+}
+
+int main(){
+
+    std::cout << append_hello("Good morning ") << std::endl;
+
+    return EXIT_SUCCESS;
+}
