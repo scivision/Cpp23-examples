83ce612fc01899f7ac8e14b892dbfa9104f467d5
scivision <scivision@users.noreply.github.com>
2023-08-15 21:46:41 -0400

    c++11 iota

---
+++ cpp11/iota.cpp
@@ -0,0 +1,17 @@
+// fill an array with consecutive numbers starting at 0, intead of explicit for loop
+#include <iostream>
+#include <numeric>
+#include <cstdlib>
+
+int main()
+{
+    int a[10];
+
+    std::iota(std::begin(a), std::end(a), 0);
+
+    for (auto i : a)
+        std::cout << i << " ";
+    std::cout << std::endl;
+
+    return EXIT_SUCCESS;
+}
