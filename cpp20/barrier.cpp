5582496c7b3d120d04b8fe5804c277c0de9cd63b
scivision <scivision@users.noreply.github.com>
2024-05-21 23:41:11 -0400

    add C++20 barrier

---
+++ cpp20/barrier.cpp
@@ -0,0 +1,46 @@
+#include <barrier>
+
+#include <iostream>
+#include <string>
+#include <thread>  // std::jthread
+#include <vector>
+#include <cstdlib>
+
+#include <syncstream>
+
+
+int main()
+{
+    const auto workers = {"Anil", "Busara", "Carl"};
+
+    auto on_completion = []() noexcept
+    {
+        // locking not needed here
+        static auto phase =
+            "... done\n"
+            "Cleaning up...\n";
+        std::cout << phase;
+        phase = "... done\n";
+    };
+
+    std::barrier sync_point(std::ssize(workers), on_completion);
+
+    auto work = [&](std::string name)
+    {
+        std::string product = "  " + name + " worked\n";
+        std::osyncstream(std::cout) << product;  // ok, op<< call is atomic
+        sync_point.arrive_and_wait();
+
+        product = "  " + name + " cleaned\n";
+        std::osyncstream(std::cout) << product;
+        sync_point.arrive_and_wait();
+    };
+
+    std::cout << "Starting...\n";
+    std::vector<std::jthread> threads;
+    threads.reserve(std::size(workers));
+    for (auto const& worker : workers)
+        threads.emplace_back(work, worker);
+
+    return EXIT_SUCCESS;
+}
