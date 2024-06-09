ee23886cc02c81a7b6a3ede569477d95d04e4a94
Michael Hirsch <scivision@users.noreply.github.com>
2024-06-09 08:02:53 -0400

    remote unneeded workarounds

--- cpp26/format_pointer.cpp
+++ cpp26/format_pointer.cpp
@@ -1,3 +1,5 @@
+// https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2022/p2510r3.pdf
+
 #include <cstdlib>
 
 #include <cstdint>
@@ -5,6 +7,7 @@
 
 #include <format>
 
+// https://en.cppreference.com/w/cpp/utility/format
 #if __cpp_lib_format < 202304L
 #error "std::format not available for pointers"
 #endif
