a613af5aa6784579a3a6b99e1d7144e41fa6de60
scivision <scivision@users.noreply.github.com>
2024-07-06 22:13:54 -0400

    check cstdlib in case of misconfigured complier environment

--- cmake/compilers.cmake
+++ cmake/compilers.cmake
@@ -2,6 +2,11 @@ include(CheckSourceCompiles)
 include(CheckCXXSymbolExists)
 include(CheckIncludeFileCXX)
 
+check_include_file_cxx(cstdlib HAVE_CSTDLIB)
+if(NOT HAVE_CSTDLIB)
+  message(FATAL_ERROR "cstdlib not found. If using a custom built compiler, ensure that compiler's include dirs are set in environment variable CPLUS_INCLUDE_PATH")
+endif()
+
 if(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang|^Intel")
   add_compile_options(-Wall -Wextra)
 endif()
